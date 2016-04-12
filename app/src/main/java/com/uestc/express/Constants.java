c43fd6d1cb1ca42f5c7b2666d980cd697e5fceb8
Tobb Huang <qaz589@qq.com>
2016-04-12 11:19:55 +0800

    init

---
+++ app/src/main/java/com/uestc/express/Constants.java
@@ -0,0 +1,7 @@
+package com.uestc.express;
+
+/**
+ * Created by Tobb_Huang on 16/4/12.
+ */
+public class Constants {
+}
---
+++ app/src/main/java/com/uestc/express/avtivity/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.uestc.express.avtivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+import com.uestc.express.R;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                LoginActivity.startActivity(SplashActivity.this);
+                finish();
+            }
+        },1500);
+
+    }
+}
